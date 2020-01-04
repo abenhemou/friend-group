cd0f6a2c4537d07198488c5b9292a7152bd33995
Asmae <2118786b@gmail.com>
2020-01-04 11:59:21 +0000

    group file final branch

--- group.py
+++ group.py
@@ -1,5 +1,29 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+# This is a friend group 
+Jill={
+    'Age':26,
+    'Job':'biologist',
+    'relationships' : {'friend': 'Zalika',
+    'partner':'John','cousin':'NA','landlord':'NA'}
+    }
+Zalika={
+    'Age':28,
+    'Job':'artist',
+    'relationships' : {'friend': 'Jill',
+    'partner':'NA','cousin':'NA','landlord':'NA'}
+    }
+John={
+    'Age':27,
+    'Job':'writer',
+    'relationships' : {'friend': 'NA',
+    'partner':'Jill','cousin':'NA','landlord':'NA'}
+    }
+Nash={
+    'Age':34,
+    'Job':'chef',
+    'relationships' : {'friend': 'NA',
+    'partner':'NA','cousin':'Jill','landlord':'Zalika'}
+    }
 
-my_group =
+people=[Jill,Zalika,Nash,John] 
